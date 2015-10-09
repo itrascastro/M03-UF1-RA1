c2b01a8be4aae45a8e26fccda1c06d173fec3114
itrascastro <i.trascastro@gmail.com>
2015-10-09 19:18:12 +0200

    Expresiones Booleanas

---
+++ UF1-RA1/ExpresionesBooleanas.vb
@@ -0,0 +1,10 @@
+﻿Public Class ExpresionesBooleanas
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim expresion As Boolean
+        Dim a As Boolean = False
+        Dim b As Boolean = True
+
+        expresion = Not (2 < 6 Or (2 * 7 > (24 / 2)))
+        MessageBox.Show(expresion)
+    End Sub
+End Class
--- UF1-RA1/My Project/Application.Designer.vb
+++ UF1-RA1/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.UF1_RA1.Aleatorio
+            Me.MainForm = Global.UF1_RA1.ExpresionesBooleanas
         End Sub
     End Class
 End Namespace
