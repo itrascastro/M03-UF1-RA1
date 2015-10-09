bd647b080cad77b805de14343bd431be9e538515
itrascastro <i.trascastro@gmail.com>
2015-10-09 17:04:20 +0200

    Aleatorio

---
+++ UF1-RA1/Aleatorio.vb
@@ -0,0 +1,12 @@
+﻿Public Class Aleatorio
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim min As Integer
+        Dim max As Integer
+        Dim r As New Random
+
+        min = CInt(TextBox1.Text)
+        max = CInt(TextBox2.Text)
+
+        TextBox3.Text = r.Next(min, max)
+    End Sub
+End Class
--- UF1-RA1/My Project/Application.Designer.vb
+++ UF1-RA1/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.UF1_RA1.Form2
+            Me.MainForm = Global.UF1_RA1.Aleatorio
         End Sub
     End Class
 End Namespace
