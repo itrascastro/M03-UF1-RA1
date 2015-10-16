61c970169537c6b22387d79a946b50ebe741eac7
itrascastro <i.trascastro@gmail.com>
2015-10-16 17:07:14 +0200

    Tabla de Multiplicar

---
+++ UF1-RA1/TablaMultiplicar.vb
@@ -0,0 +1,19 @@
+﻿Public Class TablaMultiplicar
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim num As Integer
+        Dim salida As String = ""
+        Dim cnt As Integer
+        Dim parcial As Integer
+
+        num = TextBox1.Text
+
+        For cnt = 0 To 10
+            parcial = num * cnt
+            salida = salida + num.ToString() + " x " + cnt.ToString() + " = " +
+                     parcial.ToString() + Environment.NewLine
+        Next
+
+        MessageBox.Show(salida)
+
+    End Sub
+End Class
